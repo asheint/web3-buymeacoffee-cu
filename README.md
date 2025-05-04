1b3fe9045a1586457223bb8a1733c77bc1ea2a99
Ashein00 <ashen.induwara@hotmail.com>
2025-05-04 14:13:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,8 @@
+# WHAT ARE WE MAKING?
+- Minimal HTML/JS Site
+- That has following buttons which map to the solidity smart contract:
+    - Connect
+    - Buy coffee
+    - Get Balance
+    - Withdraw
+    
---
+++ index-js.js
@@ -0,0 +1,19 @@
+import { createWalletClient, custom } from "https://esm.sh/viem";
+
+const connectButton = document.getElementById("connectButton");
+
+let walletClient;
+
+async function connect() {
+  if (typeof window.ethereum !== "undefined") {
+    walletClient = createWalletClient({
+      transport: custom(window.ethereum),
+    });
+    await walletClient.requestAddresses();
+    connectButton.innerHTML = "Connected!";
+  } else {
+    connectButton.innerHTML = "Please install MetaMask!";
+  }
+}
+
+connectButton.onclick = connect;
